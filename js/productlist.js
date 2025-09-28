88fa7075e7ed99573b5175c1c9cf5b5795f5a9a8
Noraa <noha0002@stud.kea.dk>
2025-09-28 22:21:41 +0200

    ændring

--- js/productlist.js
+++ js/productlist.js
@@ -1,1 +1,33 @@
-console.log("plist")
+const productListContainer = document.querySelector("#main");
+
+fetch(`https://kea-alt-del.dk/t7/api/products`)
+  .then((response) => response.json())
+  .then((products) => showProducts(products));
+
+function showProducts(products) {
+  console.log(products); 
+products.forEach(element => {
+    console.log(element);
+
+    productListContainer.innerHTML += `
+      <article class="udsolgt">
+         <img class="sold-edit" 
+         src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp" alt="">
+
+         <h2>${productdisplayname}</h2>
+        <p>Tshirts i Nike</p>
+        <p > Prev. DKK 1595,-</p>
+        <span class="tilbud">-34</span>
+        <p >Now Prev. DKK 1595,-</p>
+
+        <a href="Produkt.html">
+         <p class="underline">Read More</p>
+        </a>
+        
+       <span class="sold">udsolgt</span>
+       
+
+    </article
+    `;
+  });
+}
--- js/single.js
+++ js/single.js
@@ -1,41 +1,42 @@
 const productContainer = document.querySelector("#productContainer");
 const params = new URLSearchParams(window.location.search);
-const id = params.get("id");
+const id = params.get("id") || 1163; 
 
-fetch(`https://kea-alt-del.dk/t7/api/products/1163.webp`)
-  .then((res) => res.json())
-  .then((product) => {
-    console.log(product.articletype);
-    function showProduct(product) {}
-    productContainer.innerHTML = `
-      <section class="grid3">
-        <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt=""> 
-        <div>
-          <h2 class="stor1">Product Information</h2>
-          <h2 class="stor2">Model Name</h2>
-          <p>${product.productdisplayname}</p>
-          <h2 class="stor2">Color</h2>
-          <p>${product.color}</p>
-          <h2 class="stor2">Inventory Number</h2>
-          <p>${product.id}</p>
-          <h1>${product.brandname}</h1>
-          <p>${product.brandname}, creating experiences for today's athlete</p>
-        </div>
+fetch(`https://kea-alt-del.dk/t7/api/products/${id}`)
+  .then((response) => response.json())
+  .then((data) => showProduct(data))
+  
 
-        <form class="grey">
-          <h1>${product.productdisplayname}</h1>
-          <p>${product.articletype}</p>
+function showProduct(product) {
+  productContainer.innerHTML += `
+    <section class="grid3">
+      <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt=""> 
+      <div>
+        <h2 class="stor1">Product Information</h2>
+        <h2 class="stor2">Model Name</h2>
+        <p>${product.productdisplayname}</p>
+        <h2 class="stor2">Color</h2>
+        <p>${product.color}</p>
+        <h2 class="stor2">Inventory Number</h2>
+        <p>${product.id}</p>
+        <h1>${product.brandname}</h1>
+        <p>${product.brandname}, creating experiences for today's athlete</p>
+      </div>
 
-          <label for="size">Choose a size:</label>
-          <select id="size">
-            <option value="">Select</option>
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
-          </select>
+      <form class="grey">
+        <h1>${product.productdisplayname}</h1>
+        <p>${product.articletype}</p>
 
-          <button type="button" id="addToCart">Put in basket</button>
-        </form>
-      </section>
-    `;
-  });
+        <label for="size">Choose a size:</label>
+        <select id="size">
+          <option value="">Select</option>
+          <option value="small">Small</option>
+          <option value="medium">Medium</option>
+          <option value="large">Large</option>
+        </select>
+
+        <button type="button" id="addToCart">Put in basket</button>
+      </form>
+    </section>
+  `;
+}
